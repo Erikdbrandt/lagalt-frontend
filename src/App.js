49911f3ce750292000a12947d765884a4508a53d
Arif Hossain <hossaininfo@ymail.com>
2023-03-16 08:29:17 +0100

    added keycloak that does not work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EditProductPage from "./pages/EditProductPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/navbar/Navbar";
+import KeycloakRoute from "./routes/KeycloakRoute";
+import { ROLES } from "./const/roles";
 import Header from "./Header";
 import MainPage from "./views/MainPage";
 
 function App() {
     return (
-        <div className="App">
-
-            <Header/>
-            <MainPage/>
-
-
-        </div>
+        <BrowserRouter>
+            <Navbar />
+            <main className="container">
+                <Routes>
+                    <Route path="/" element={<Header />} />
+                    <Route path="/products" element={<MainPage />} />
+                    {/*<Route path="/products/:productId" element={<EditProductPage />} />*/}
+                    <Route
+                        path="/profile"
+                        element={
+                            <KeycloakRoute role={ ROLES.User }>
+                                <ProfilePage />
+                            </KeycloakRoute>
+                        }
+                    />
+                </Routes>
+            </main>
+        </BrowserRouter>
+        // <div className="App">
+        //
+        //     <Header/>
+        //     <MainPage/>
+        //
+        //
+        // </div>
     );
 }
 
