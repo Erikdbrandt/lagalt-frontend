57d4213fd51218ebb08302dbe843b9ca44eb6118
Zakhida Ruslanova <zakhida_sul@hotmail.se>
2023-03-23 08:49:39 +0100

    Added funktion to add a skill and popup message when a project is created.

--- src/views/NewProject.jsx
+++ src/views/NewProject.jsx
@@ -2,31 +2,47 @@ import {PlusOutlined} from '@ant-design/icons';
 import {
     Button,
     Checkbox,
-    DatePicker,
     Form,
     Input,
-    InputNumber,
     Radio,
     Switch,
     Upload,
+    Space,
+    Modal
 } from 'antd';
-import {useState, useEffect} from 'react';
-import {Modal} from 'antd';
+import {useState, useEffect } from 'react';
 import axios from 'axios';
 
-const {RangePicker} = DatePicker;
 const {TextArea} = Input;
-const {confirm} = Modal;
+;
 const NewProject = () => {
     const [componentDisabled, setComponentDisabled] = useState(true);
-    const [skillNames, setSkillNames] = useState([]);
+    const [skill, setSkill] = useState([]);
     const [form] = Form.useForm();
+    const [showCreateSkillForm, setShowCreateSkillForm] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handleCreateSkill = async (values) => {
+        try {
+            const response = await axios.post('http://localhost:8080/api/v1/skill/create', values);
+            console.log(response.data);
+            form.resetFields();
+            setSkill([...skill, values.name]);
+            setShowCreateSkillForm(false);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
         async function fetchSkillNames() {
-            const response = await axios.get('http://localhost:8080/api/v1/skill');
-            const skillNames = response.data.map(skill => skill.name);
-            setSkillNames(skillNames);
+            try {
+                const response = await axios.get('http://localhost:8080/api/v1/skill');
+                const skillNames = response.data.map(skill => skill.name);
+                setSkill(skillNames);
+            } catch (error) {
+                console.error(error);
+            }
         }
 
         fetchSkillNames();
@@ -34,8 +50,10 @@ const NewProject = () => {
     const handleSubmit = async (values) => {
         try {
             const response = await axios.post('http://localhost:8080/api/v1/project/create', values);
-            console.log(response.data); // do something with the response
-            form.resetFields(); // clear the form fields
+            console.log(response.data);
+            form.resetFields();
+            setIsModalVisible(true);
+
         } catch (error) {
             console.error(error);
         }
@@ -49,6 +67,59 @@ const NewProject = () => {
             >
                 Form disabled
             </Checkbox>
+
+            <div className="mt-3">
+                <Form.Item
+                    label="Skills"
+                    name="skills"
+                    valuePropName="checked"
+                >
+
+                    <Checkbox.Group style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+                        {skill.map(skillName => (
+                            <Checkbox key={skillName} value={skillName} className="mb-2">
+                                {skillName}
+                            </Checkbox>
+                        ))}
+
+                    </Checkbox.Group>
+
+                    <div className="mt-5">
+                        {showCreateSkillForm ? (
+                            <Form
+                                onFinish={handleCreateSkill}
+                                name="wrap"
+                                labelCol={{flex: '110px'}}
+                                labelAlign="left"
+                                labelWrap
+                                wrapperCol={{flex: 1}}
+                                colon={false}
+                                style={{maxWidth: 600}}
+                            >
+                                <Form.Item label="Name" name="name" rules={[{required: true}]}>
+                                    <Input/>
+                                </Form.Item>
+
+                                <Form.Item label="Description" name="description">
+                                    <Input/>
+                                </Form.Item>
+
+                                <Form.Item label=" ">
+                                    <Button htmlType="submit">
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        ) : (
+                            <Space wrap className="mt-3">
+
+                                <Button type="dashed" onClick={() => setShowCreateSkillForm(true)}>Add new
+                                    skill</Button>
+                            </Space>
+                        )}
+                    </div>
+                </Form.Item>
+            </div>
             <Form
                 onFinish={handleSubmit}
                 labelCol={{
@@ -63,21 +134,7 @@ const NewProject = () => {
                     maxWidth: 600,
                 }}
             >
-                <div className="mt-5">
-                    <Form.Item
-                        label="Skills"
-                        name="skills"
-                        valuePropName="checked"
-                    >
-                        <Checkbox.Group>
-                            {skillNames.map(skillName => (
-                                <Checkbox key={skillName} value={skillName}>
-                                    {skillName}
-                                </Checkbox>
-                            ))}
-                        </Checkbox.Group>
-                    </Form.Item>
-                </div>
+
                 <Form.Item label="Type">
                     <Radio.Group>
                         <Radio value="MOVIE"> MOVIE </Radio>
@@ -92,15 +149,6 @@ const NewProject = () => {
                 <Form.Item label="Theme" name="theme">
                     <Input/>
                 </Form.Item>
-                {/*<Form.Item label="DatePicker">*/}
-                {/*    <DatePicker/>*/}
-                {/*</Form.Item>*/}
-                {/*<Form.Item label="RangePicker">*/}
-                {/*    <RangePicker/>*/}
-                {/*</Form.Item>*/}
-                {/*<Form.Item label="InputNumber">*/}
-                {/*    <InputNumber/>*/}
-                {/*</Form.Item>*/}
                 <Form.Item label="Description" name="description">
                     <TextArea rows={4}/>
                 </Form.Item>
@@ -124,6 +172,16 @@ const NewProject = () => {
                 <Form.Item>
                     <Button htmlType="submit">Create</Button>
                 </Form.Item>
+                <Modal
+                    title="Project Created"
+                    visible={isModalVisible}
+                    onOk={() => {
+                        setIsModalVisible(false);
+                    }}
+                    onCancel={() => setIsModalVisible(false)}
+                >
+                    <p>The project has been successfully created.</p>
+                </Modal>
             </Form>
         </>
     );
