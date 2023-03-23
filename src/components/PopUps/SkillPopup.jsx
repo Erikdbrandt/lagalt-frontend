2934fb065d6897c420643aac0e02257369c65803
Erik Brandt <erikdbrandt@gmail.com>
2023-03-23 14:37:08 +0100

    work on SkillPopup.jsx and made UserProfile.jsx display skills in a eyecatching way

--- src/components/PopUps/SkillPopup.jsx
+++ src/components/PopUps/SkillPopup.jsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { getAllSkills } from "../../api/skills";
+import React, {useEffect, useState} from "react";
+import {getAllSkills} from "../../api/skills";
 import AddSkillPopup from "./AddSkillPopup"
+import {useUser} from "../context/UserContext";
 
 
-const SkillPopup = ({ onSaveSkills, onCancel }) => {
+const SkillPopup = ({onSaveSkills, onCancel}) => {
     const [allSkills, setAllSkills] = useState([]);
     const [selectedSkills, setSelectedSkills] = useState([]);
+    const {user, handleUpdateUser} = useUser();
+
 
     const [showAddSkillPopup, setShowAddSkillPopup] = useState(false);
 
+   // const [usersInitialSkills, setUsersInitialSkills] = useState([]);
 
     useEffect(() => {
         async function fetchSkills() {
             const [error, skills] = await getAllSkills();
+
+            console.log(user.skills)
 
             if (error) {
                 console.log(error);
@@ -20,23 +26,50 @@ const SkillPopup = ({ onSaveSkills, onCancel }) => {
             }
 
             setAllSkills(skills);
+
+            if (user.skills) {
+
+                const skillsToAdd = skills.filter(skill => user.skills.includes(skill.skill_id));
+                console.log(skillsToAdd)
+
+                setSelectedSkills(skills.filter(skill => user.skills.includes(skill.skill_id)));
+            }
         }
 
-
         fetchSkills();
-    }, []);
+    }, [user]);
 
     const handleCheckboxChange = (event) => {
         const skillId = event.target.id;
         const skillName = event.target.name;
         const checked = event.target.checked;
 
+        // Create a new array of selected skills that includes any previously selected skills
+        let newSelectedSkills = [...selectedSkills];
+
+        console.log(checked)
+
         if (checked) {
-            setSelectedSkills([...selectedSkills, { skill_id: skillId, name: skillName }]);
+            newSelectedSkills.push({ skill_id: skillId, name: skillName });
+
         } else {
-            setSelectedSkills(selectedSkills.filter((skill) => skill.skill_id !== skillId));
+            console.log("HI")
+            newSelectedSkills = newSelectedSkills.filter(
+                (selectedSkill) => selectedSkill.skill_id != skillId
+            );
+
+
+
+
         }
+
+        console.log(newSelectedSkills)
+
+        setSelectedSkills(newSelectedSkills);
     };
+
+
+
 
     const handleSave = () => {
 
@@ -48,7 +81,6 @@ const SkillPopup = ({ onSaveSkills, onCancel }) => {
     }
 
     const handleAddSkill = (newSkill) => {
-
 
 
         setAllSkills([...allSkills, newSkill]);
@@ -68,7 +100,7 @@ const SkillPopup = ({ onSaveSkills, onCancel }) => {
                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
 
                 <div
-                    className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-md sm:w-full max-h-96 "
+                    className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-md sm:w-full max-h-fit "
                     role="dialog"
                     aria-modal="true"
                     aria-labelledby="modal-headline"
@@ -78,9 +110,10 @@ const SkillPopup = ({ onSaveSkills, onCancel }) => {
                             className="w-full mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left"
                             id="modal-headline"
                         >
-                            <h2 className="text-lg font-medium text-gray-900 mb-3">
+                            <h2 className="text-lg font-medium text-gray-900 mb-1">
                                 Select your skills:
                             </h2>
+
                             {allSkills.map((skill) => (
                                 <div key={skill.skill_id}>
                                     <label className="inline-flex items-center mt-3">
@@ -90,26 +123,35 @@ const SkillPopup = ({ onSaveSkills, onCancel }) => {
                                             id={skill.skill_id}
                                             name={skill.name}
                                             onChange={handleCheckboxChange}
+                                            checked={
+
+                                                (selectedSkills.some((selectedSkill) => selectedSkill.skill_id == skill.skill_id))
+                                            }
+
                                         />
+
+
                                         <span className="ml-2 text-gray-700">{skill.name}</span>
                                     </label>
                                 </div>
                             ))}
+
+
                         </div>
                     </div>
                     {showAddSkillPopup ? (
-                        <AddSkillPopup onAddSkill={handleAddSkill} onCancel={() => setShowAddSkillPopup(false)} />
+                        <AddSkillPopup onAddSkill={handleAddSkill} onCancel={() => setShowAddSkillPopup(false)}/>
                     ) : (
                         <button
                             type="button"
                             className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-500 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm mt-4"
                             onClick={() => setShowAddSkillPopup(true)}
                         >
-                            Add Skill
+                            Add A New Skill
                         </button>
                     )}
 
-                    <div className="mt-2 sm:mt-2 sm:flex sm:flex-row-reverse">
+                    <div className="mt-2  sm:mt-2 sm:flex sm:flex-row-reverse">
                         <button
                             type="button"
                             className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
@@ -117,14 +159,15 @@ const SkillPopup = ({ onSaveSkills, onCancel }) => {
                         >
                             Save
                         </button>
-                        <button
-                            type="button"
-                            className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm"
-
-                            onClick={handleCancel}
-                        >
-                            Cancel
-                        </button>
+                        <div className="mt-3 sm:mt-0 sm:w-auto sm:text-sm">
+                            <button
+                                type="button"
+                                className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                                onClick={handleCancel}
+                            >
+                                Cancel
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/views/UserProfile.jsx
+++ src/views/UserProfile.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useUser } from "../components/context/UserContext";
-import {updateSkillsInUser, updateUser} from "../api/userService";
+import { updateSkillsInUser, updateUser } from "../api/userService";
 import SkillPopup from "../components/PopUps/SkillPopup";
+import { getAllSkills } from "../api/skills";
 
 const UserProfile = () => {
     const { user, handleUpdateUser } = useUser();
     const [visibility, setVisibility] = useState(user.userVisibility);
     const [showPopup, setShowPopup] = useState(false);
+    const [updatedSkills, setUpdatedSkills] = useState(user.skills || []);
+
+    useEffect(() => {
+        async function fetchSkills() {
+            const [error, skills] = await getAllSkills();
+
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            if (user.skills) {
+                setUpdatedSkills(skills.filter(skill => user.skills.includes(skill.skill_id)));
+            }
+        }
+
+        fetchSkills();
+
+        setUpdatedSkills(user.skills || []);
+    }, [user.skills]);
 
     const toggleVisibility = async () => {
         const updatedVisibility = visibility === "REGULAR" ? "HIDDEN" : "REGULAR";
@@ -28,12 +49,12 @@ const UserProfile = () => {
         setShowPopup(!showPopup);
     };
 
-    const handleSaveSkills = async (selectedSkills) => {
+    const handleSaveSkills = async selectedSkills => {
         // Create a new array of skill IDs
         const skillIds = selectedSkills.map(skill => skill.skill_id);
 
-        // Combine the selected skill IDs array with the existing user.skills array
-        const updatedSkills = [...user.skills, ...skillIds];
+        // Combine the selected skill IDs array with the existing user.skills array, or use an empty array if user.skills is null or undefined
+        const updatedSkills = [...skillIds];
 
         // Call the API to update the user's skills
         const [error, updatedUser] = await updateSkillsInUser(user.user_id, updatedSkills);
@@ -44,6 +65,7 @@ const UserProfile = () => {
         }
 
         handleUpdateUser(updatedUser);
+        setUpdatedSkills(selectedSkills);
 
         togglePopup();
     };
@@ -53,23 +75,56 @@ const UserProfile = () => {
     };
 
     return (
-        <div>
-            <h1>Profile Page</h1>
+        <div className="mx-8 ">
+
+
+            <h1 className="text-3xl font-bold my-8">Profile Page</h1>
             {user && (
                 <div>
-                    <h4>User</h4>
-                    <p>Name: {user.full_name}</p>
-                    <p>Username: {user.email}</p>
-                    <p>
-                        Visibility: {user.userVisibility === "REGULAR" ? "Visible" : "Hidden"}{" "}
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={toggleVisibility}>
+                    <div className="p-4 bg-white rounded-lg shadow-md border-t-4 border-blue-500">
+
+                    <p className="text-gray-700 mb-2">
+                        <span className="font-bold">Name:</span> {user.full_name}
+                    </p>
+                    <p className="text-gray-700 mb-2">
+                        <span className="font-bold">Username:</span> {user.email}
+                    </p>
+                    <p className="text-gray-700 mb-4">
+                        <span className="font-bold">Visibility:</span>{" "}
+                        {user.userVisibility === "REGULAR" ? "Visible" : "Hidden"}{" "}
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
+                            onClick={toggleVisibility}
+                        >
                             {user.userVisibility === "REGULAR" ? "Hide" : "Show"}
                         </button>
                     </p>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={togglePopup}>
-                        Show Skills
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={togglePopup}
+                    >
+                        Edit Skills
                     </button>
-                    {showPopup && <SkillPopup onSaveSkills={handleSaveSkills} onCancel={handleCancelSkills} />}
+                    </div>
+
+                    {updatedSkills.length > 0 && (
+                        <div>
+                            <h4 className="text-lg font-medium my-4">Skills</h4>
+                            <ul className="grid grid-cols-3 gap-4">
+                                {updatedSkills.map(skill => (
+                                    <li key={skill.skill_id}>
+                                        <div className="p-4 bg-white rounded-lg shadow-md border-t-4 border-blue-500">
+                                            <h5 className="text-lg font-medium mb-2">{skill.name}</h5>
+                                            <p className="text-gray-700">{skill.description}</p>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                    {showPopup && (
+                        <SkillPopup onSaveSkills={handleSaveSkills} onCancel={handleCancelSkills} />
+                    )}
                 </div>
             )}
         </div>
